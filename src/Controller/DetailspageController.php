c56c742c10be82880e1254a5fb0409ac5ce0b98c
ma77hieu <mat.ortlieb@gmail.com>
2021-10-25 16:55:53 +0200

    trickdetail page now also supports edition when called by the edition route

--- src/Controller/DetailspageController.php
+++ src/Controller/DetailspageController.php
@@ -12,18 +12,47 @@ class DetailspageController extends AbstractController
     public function show(): Response
     {
         $tricks=1;
+        $user_logged_in=null;
         $medias=[1,1,1,1,1];
-        $videos=null;//tests purpose
-        $images=1;//tests purpose
+        $videos=1;//tests purpose
+        $images=null;//tests purpose
         $tags=[1,1,1];//tests purpose
         $comments=[1,1,1];//tests purpose
+        $edition_mode=false;
         return $this->render('tricks/tricksDetails.html.twig',[
             'tricks'=>$tricks,
             'medias'=>$medias,
             'videos'=>$videos,
             'images'=>$images,
             'tags'=>$tags,
-            'comments'=>$comments]);
+            'comments'=>$comments,
+            'user_logged_in'=>$user_logged_in,
+            'edition_mode'=>$edition_mode]);
+    }
+
+    public function edit(): Response
+    {
+        $tricks=1;
+        $user_logged_in=null;
+        $medias=[1,1,1,1,1];
+        $videos=1;//tests purpose
+        $images=null;//tests purpose
+        $tags=[1,1,1];//tests purpose
+        $comments=[1,1,1];//tests purpose
+        $edition_mode=true;
+        $description="tecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores e aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+        $groupes=['Groupe A','Groupe B','Groupe C'];
+        return $this->render('tricks/tricksDetails.html.twig',[
+            'tricks'=>$tricks,
+            'medias'=>$medias,
+            'videos'=>$videos,
+            'images'=>$images,
+            'tags'=>$tags,
+            'comments'=>$comments,
+            'user_logged_in'=>$user_logged_in,
+            'edition_mode'=>$edition_mode,
+            'description'=>$description,
+            'groups'=>$groupes]);
     }
 
 }
