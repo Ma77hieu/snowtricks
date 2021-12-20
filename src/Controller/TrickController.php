77d3a177b226991cb79f049e8b16ffaab5f5c92e
ma77hieu <mat.ortlieb@gmail.com>
2021-12-20 16:43:59 +0100

    pages for: Homepage / trick details / trick edition now share the same hero section twig file

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MediaRepository;
 use App\Services\TrickServices;
 use Symfony\Component\Security\Core\Security;
 use App\Entity\Group;
@@ -15,8 +16,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
 
 
 class TrickController extends AbstractController
@@ -44,7 +43,7 @@ class TrickController extends AbstractController
         $form = $this->createForm(TrickFormType::class, $trick);
         $form->handleRequest($request);
         $entityManager = $this->getDoctrine()->getManager();
-        $serviceReturn=$this->trickServices->createTrick($entityManager,$trick,$form);
+        $serviceReturn = $this->trickServices->createTrick($entityManager, $trick, $form);
         /*$trickService= new TrickServices();
         $serviceReturn=$trickService->createTrick($request);*/
         return $this->controllerReturn($serviceReturn);
@@ -59,18 +58,16 @@ class TrickController extends AbstractController
     {
         $comment = new Comment();
         $entityManager = $this->getDoctrine()->getManager();
-        $trick = $entityManager->find(Trick::class,$trickId);
+        $trick = $entityManager->find(Trick::class, $trickId);
         $group = $entityManager->find(Group::class, $trick->getTrickGroup());
-        $form = $this->createForm(CommentFormType::class,$comment);
+        $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
-        $trickService= new TrickServices();
+        $trickService = new TrickServices();
         /*$serviceReturn=$trickService->showTrick($entityManager,$trick,$group,$form,$trickId,$comment);
         return $this->controllerReturn($serviceReturn);*/
-        if ($form->isSubmitted())
-        {
+        if ($form->isSubmitted()) {
 
-            if ($form->isValid())
-            {
+            if ($form->isValid()) {
                 $user = $this->getUser();
 
                 $comment->setCreationDate(new \DateTime());
@@ -81,42 +78,43 @@ class TrickController extends AbstractController
                 $entityManager->flush();
                 $this->addFlash('success', 'Votre commentaire a été soumis, il est en attente de validation de notre équipe.');
 
-            }
-            else
-            {
+            } else {
                 $errors = $form->getErrors();
                 $this->addFlash('danger', "$errors");
             }
         }
         $mediaRepository = $this->getDoctrine()->getRepository(Media::class);
         $medias = $mediaRepository->findByTrickId($trickId);
-        $mainMediaUrl=null;
+        /*$mainMediaUrl=null;
         $mainMediaId=null;
         foreach ($medias as $media){
             if($media->getIsMain()){
                 $mainMediaUrl=$media->getUrl();
                 $mainMediaId=$media->getId();
             }
-        }
+        }*/
+        $mainMedia = $this->getMediaUrlAndId($medias);
+        $mainMediaUrl = $mainMedia['mediaUrl'];
+        $mainMediaId = $mainMedia['mediaId'];
         $commentRepository = $this->getDoctrine()->getRepository(Comment::class);
         $comments = $commentRepository->findByTrickId($trickId);
-        $tags=[
-            'date de creation'=>$trick->getCreationDate()->format('Y-m-d H:i:s'),
-            'groupe'=>$group->getName(),
+        $tags = [
+            'date de creation' => $trick->getCreationDate()->format('Y-m-d H:i:s'),
+            'groupe' => $group->getName(),
         ];
         if ($trick->getModificationDate()) {
             $trickModifDate = $trick->getModificationDate()->format('Y-m-d H:i:s');
-            $tags['date de modification']=$trickModifDate;
+            $tags['date de modification'] = $trickModifDate;
         }
 
-        return $this->render('tricks/trickDetails.html.twig',[
+        return $this->render('tricks/trickDetails.html.twig', [
             'commentForm' => $form->createView(),
             'medias' => $medias,
             'comments' => $comments,
             'trick' => $trick,
-            'tags'=>$tags,
-            'mainMediaUrl'=>$mainMediaUrl,
-            'mainMediaId'=>$mainMediaId]);
+            'tags' => $tags,
+            'mainMediaUrl' => $mainMediaUrl,
+            'mainMediaId' => $mainMediaId]);
     }
 
     /**
@@ -127,40 +125,41 @@ class TrickController extends AbstractController
     public function edit(int $trickId, Request $request): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
-        $trick = $entityManager->find(Trick::class,$trickId);
+        $trick = $entityManager->find(Trick::class, $trickId);
         $form = $this->createForm(TrickFormType::class, $trick);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted())
-        {
+        if ($form->isSubmitted()) {
             /*            echo ("form valide?");
                         var_dump($form->isValid());*/
-            if ($form->isValid())
-            {
+            if ($form->isValid()) {
                 $entityManager = $this->getDoctrine()->getManager();
                 $trick->setModificationDate(new \DateTime());
                 $entityManager->merge($trick);
                 $entityManager->flush();
                 $this->addFlash('success', 'Trick mis à jour.');
                 return $this->redirectToRoute('index');
-            }
-            else
-            {
+            } else {
                 $errors = $form->getErrors();
                 $this->addFlash('danger', "$errors");
             }
         }
         $mediaRepository = $this->getDoctrine()->getRepository(Media::class);
         $medias = $mediaRepository->findBy(['trick' => $trickId]);
+        $mainMedia = $this->getMediaUrlAndId($medias);
+        $mainMediaUrl = $mainMedia['mediaUrl'];
+        $mainMediaId = $mainMedia['mediaId'];
         $groupRepository = $this->getDoctrine()->getRepository(Group::class);
         $groups = $groupRepository->findAll();
 
         /*var_dump($groups);die;*/
-        return $this->render('tricks/trickEdition.html.twig',[
+        return $this->render('tricks/trickEdition.html.twig', [
             'trickForm' => $form->createView(),
             'medias' => $medias,
             'trick' => $trick,
-            'groups' => $groups]);
+            'groups' => $groups,
+            'mainMediaUrl' => $mainMediaUrl,
+            'mainMediaId' => $mainMediaId]);
     }
 
     /**
@@ -169,23 +168,40 @@ class TrickController extends AbstractController
      */
     public function controllerReturn($input)
     {
-        $returnType=$input['returnType'];
-        $path=$input['path'];
-        $flashType=$input['flashType'];
-        $flashMessage=$input['flashMessage'];
-        $data=$input['data'];
-        if ($flashMessage){
+        $returnType = $input['returnType'];
+        $path = $input['path'];
+        $flashType = $input['flashType'];
+        $flashMessage = $input['flashMessage'];
+        $data = $input['data'];
+        if ($flashMessage) {
             $this->addFlash($flashType, $flashMessage);
         }
-        if ($returnType==='render'){
-            return $this->render($path,$data);
+        if ($returnType === 'render') {
+            return $this->render($path, $data);
         }
-        if ($returnType==='redirect'){
-            return $this->redirectToRoute($path,$data);
-        }
-        else{
+        if ($returnType === 'redirect') {
+            return $this->redirectToRoute($path, $data);
+        } else {
             $this->addFlash("danger", "une erreur interne est survenue, vous avez été redirigé vers la page d'accueil");
             return $this->redirectToRoute('index');
         }
+    }
+
+    /**
+     * @param array $mediaRepository
+     * @return array
+     */
+    public function getMediaUrlAndId(Array $mediaRepository): array
+    {
+        $MediaUrl = null;
+        $MediaId = null;
+        foreach ($mediaRepository as $media) {
+            if ($media->getIsMain()) {
+                $MediaUrl = $media->getUrl();
+                $MediaId = $media->getId();
+            }
+        }
+        return (['mediaUrl' => $MediaUrl,
+            'mediaId' => $MediaId]);
     }
 }
