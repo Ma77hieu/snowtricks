7054b9f302368c97241a3cc04dad0f6ab7dbd6e0
ma77hieu <mat.ortlieb@gmail.com>
2022-03-22 11:57:42 +0100

    front improvement on trick form (increased number of lines in description field)

--- src/Form/TrickFormType.php
+++ src/Form/TrickFormType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,7 +21,10 @@ class TrickFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
+            /*->add('description')*/
+            ->add('description', TextareaType::class, [
+                'attr' => ['rows' => 4],
+            ])
             ->add('name')
             ->add('trickGroup', EntityType::class, [
                 // looks for choices from this entity
